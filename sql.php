133141a8c7bbbed8190bc1453c83f06f22355a82
Сергей Городецкий <78065813+devilnotcry77@users.noreply.github.com>
2023-03-04 00:38:30 +0300

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,28 @@
+﻿<!doctype html>
+<html lang="ru">
+<head>
+	<title>Admin_Panel</title>
+</head>
+<body>
+  <?php
+	$host = '';
+	$user = '';
+	$pass = '';
+	$db_name = '';
+	$link = mysqli_connect($host, $user, $pass, $db_name);
+
+
+      if(!$link) {
+	    echo 'I not connect to the database. Error code: ' .mysqli_connect_error().',error:'.mysqli_connect_error();
+	 	exit;
+	}
+  if (isset($_POST[""])) {
+    $sql = mysqli_query($link, "INSERT INTO `` (``, ``, ``, ``, ``) VALUES ('{$_POST['']}', '{$_POST['']}', '{$_POST['']}','{$_POST['']}','{$_POST['']}')");
+
+    if ($sql) {
+      echo '<p>Data has been successfully added to the table.</p>';
+    } else {
+      echo '<p>Sorry, error: ' . mysqli_error($link) . '</p>';
+    }
+  }
+?>
